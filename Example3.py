8abbbef368631528e18f75dd476c3e281154756f
MAX <maksimdemidovru@mail.ru>
2022-10-13 10:09:53 +0300

    add Ex3

--- Example3.py
+++ Example3.py
@@ -1,12 +1,10 @@
 string1 = input()
 string2 = input()
 
-for i in range(len(string1)):
+for i in range(len(string2)):
     count = 0
-    count2 = 1
+    m=1
     for m in range(len(string1)):
-        if count2 < len(string1):
-            if string2[i] == string1[m]:
-                count+=1 
-    count2+=1
+        if string2[i] == string1[m]:
+            count+=1 
     print(f"В первой строке буква {string2[i]} встречается {count} раз")
